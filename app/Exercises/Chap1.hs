3fefd976759d920353108319b3dee23acd0c8811
April Gonçalves <april@cyberglot.me>
2022-06-10 19:17:18 +0100

    Add Exercises and Solutions

---
+++ app/Exercises/Chap1.hs
@@ -0,0 +1,24 @@
+module Exercises.Chap1 where
+
+import ComplexNumbers
+import ComplexNumSemantics
+
+-- Exercise 1
+data Exp  =  Con Integer
+          |  Plus   Exp  Exp
+          |  Minus  Exp  Exp
+          |  Times  Exp  Exp
+  deriving (Eq, Show)
+
+
+-- Exercise 2
+data E2 a  =  Con a
+           |  Var String
+           |  Plus   (E2 a)  (E2 a)
+           |  Minus  (E2 a)  (E2 a)
+           |  Times  (E2 a)  (E2 a)
+  deriving(Eq, Show)
+
+-- Exercise 11
+embed :: Complex r -> ComplexSyn r
+embed (C (x, y)) = ToComplexCart x y
---
+++ app/Solutions/Chap1.hs
@@ -0,0 +1,25 @@
+-- No solutions yet
+module Solutions.Chap1 where
+
+import ComplexNumbers
+import ComplexNumSemantics
+
+-- Exercise 1
+data Exp  =  Con Integer
+          |  Plus   Exp  Exp
+          |  Minus  Exp  Exp
+          |  Times  Exp  Exp
+  deriving (Eq, Show)
+
+
+-- Exercise 2
+data E2 a  =  Con a
+           |  Var String
+           |  Plus   (E2 a)  (E2 a)
+           |  Minus  (E2 a)  (E2 a)
+           |  Times  (E2 a)  (E2 a)
+  deriving(Eq, Show)
+
+-- Exercise 11
+embed :: Complex r -> ComplexSyn r
+embed (C (x, y)) = ToComplexCart x y
